b175ba8fae07d8194ae166a6031c783ad7e35903
molgin <molgin@users.noreply.github.com>
2015-02-03 17:04:12 -0500

    save session matchups to user on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,9 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     
     session[:user_id] = user.id
+    Matchup.where(id: session[:matchup_ids]).update_all(user_id: user.id)
 
-    redirect_to root_path, :notice => "You have successfully signed in"
+    redirect_to :back, :notice => "You have successfully signed in"
     # binding.pry
   end
 

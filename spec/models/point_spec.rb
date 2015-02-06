245703e57201fa545425084307d0fa7799ff3934
molgin <molgin@users.noreply.github.com>
2015-02-05 19:36:55 -0500

    coupla basic point tests

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Point, :type => :model do
-  
+  it "is valid with a latitude" do
+    point = Point.create(lat: 40.763991, long: -73.827525, city_id: 1)
+    expect(point).to be_valid
+  end
+
+  it "is invalid without a latitude" do
+    point = Point.create
+    point.valid?
+    expect(point.errors[:lat]).to include("can't be blank")
+  end
 end

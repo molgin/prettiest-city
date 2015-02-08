1cd4dc6158e12a782527fb9300dbea21fe26de22
molgin <molgin@users.noreply.github.com>
2015-02-07 21:47:52 -0500

    another test

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -22,4 +22,13 @@ RSpec.describe Point, :type => :model do
     point.valid?
     expect(point.errors[:long]).to include("can't be blank")
   end
+
+  describe '#coords' do
+
+    it 'returns the coordinates as an array' do
+      point = Point.create(lat: 40.763991, long: -73.827525, city_id: 1)
+      expect(point.coords).to eq([40.763991, -73.827525])
+    end
+
+  end
 end

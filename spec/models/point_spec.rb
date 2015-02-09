15d7b29f8fd02f2ca1b3bd9b42ee0d7f5057973d
molgin <molgin@users.noreply.github.com>
2015-02-08 19:49:01 -0500

    more basic point tests

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -23,6 +23,17 @@ RSpec.describe Point, :type => :model do
     expect(point.errors[:long]).to include("can't be blank")
   end
 
+  it "is valid with a city" do
+    point = Point.create(lat: 40.763991, long: -73.827525, city_id: 1)
+    expect(point).to be_valid
+  end
+
+  it "is invalid without a city" do
+    point = Point.create
+    point.valid?
+    expect(point.errors[:city_id]).to include("can't be blank")
+  end
+
   describe '#coords' do
 
     it 'returns the coordinates as an array' do

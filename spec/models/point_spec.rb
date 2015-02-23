ca0a01d3df6009ee50d051c81ee88a5e5274dd0d
molgin <molgin@users.noreply.github.com>
2015-02-22 23:38:46 -0500

    another point test

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -42,4 +42,14 @@ RSpec.describe Point, :type => :model do
     end
 
   end
+
+  describe '#image_link' do
+
+    it 'returns the correct street view image link' do
+      point = Point.create(lat: 40.763991, long: -73.827525, city_id: 1)
+      img_link = "https://maps.googleapis.com/maps/api/streetview?size=500x500&location=40.763991,-73.827525&fov=90&heading=34&pitch=10&key=#{ENV['STREET_VIEW_KEY']}"
+      expect(point.image_link).to eq(img_link)
+    end
+
+  end
 end
